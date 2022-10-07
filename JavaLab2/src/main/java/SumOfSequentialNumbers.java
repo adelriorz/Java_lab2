6927505f5c62d61805cfebc079c236444eba9c57
Armando <adelriorz@gmail.com>
2022-10-07 01:44:02 -0500

    Fixed fibonnacci loop

--- JavaLab2/src/main/java/SumOfSequentialNumbers.java
+++ JavaLab2/src/main/java/SumOfSequentialNumbers.java
@@ -11,12 +11,14 @@ public class SumOfSequentialNumbers {
     public SumOfSequentialNumbers(){}
     
     public static int sequentialPrint(int n){
-        if(n == 0)
+        if(n == 0){
             return 0;
-        else if(n == 1)
+	}
+	if(n == 1 || n == 2)
             return 1;
-        else
-          return sequentialPrint(n - 1) + sequentialPrint(n - 2);
+        else{
+            return sequentialPrint(n-2) + sequentialPrint(n-1);
+        }
     }
     
     public static void main(String[] args) {
